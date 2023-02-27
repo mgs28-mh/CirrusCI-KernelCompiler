af880b389cb286e3bbaa9d7e8e1f0e4f1a691e16
ZilverQueen <muhammad.amier2002@gmail.com>
2023-02-28 06:29:14 +0700

    Fix typo

--- kernel_build.sh
+++ kernel_build.sh
@@ -54,13 +54,13 @@ function tg_post_msg() {
   -d "disable_web_page_preview=true" \
   -d "parse_mode=html" \
   -d text="$1"
-
 }
+
+# Post Main Information
+tg_post_msg "<b>CI Build Triggered</b>%0A<b>Docker OS : </b><code>${DISTRO}</code>%0A<b>Host Core Count : </b><code>${PROCS}</code>%0A<b>Builder Name : </b><code>${KBUILD_BUILD_USER}</code>%0A<b>Builder Host : </b><code>${KBUILD_BUILD_HOST}</code>%0A<b>Device Defconfig : </b><code>${DEVICE_DEFCONFIG}</code>%0A<b>Clang Version : </b><code>${KBUILD_COMPILER_STRING}</code>%0A<b>Clang Rootdir : </b><code>${CLANG_ROOTDIR}</code>%0A<b>Kernel Rootdir : </b><code>${KERNEL_ROOTDIR}</code>"
 
 # Compilation
 function compile() {
-# Post Main Information
-tg_post_msg "<b>CI Build Triggered</b>%0A<b>Docker OS : </b><code>${DISTRO}</code>%0A<b>Host Core Count : </b><code>${PROCS}</code>%0A</b>Builder Name : </b><code>${KBUILD_BUILD_USER}</code>%0A</b>Builder Host : </b><code>${KBUILD_BUILD_HOST}</code>%0A</b>Device Defconfig : </b><code>${DEVICE_DEFCONFIG}</code>%0A</b>Clang Version : </b><code>${KBUILD_COMPILER_STRING}</code>%0A</b>Clang Rootdir : </b><code>${CLANG_ROOTDIR}</code>%0A</b>Kernel Rootdir : </b><code>${KERNEL_ROOTDIR}</code>"
 tg_post_msg "<b>CI Build Triggered : </b><code>Compilation has started</code>"
 cd ${KERNEL_ROOTDIR}
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
